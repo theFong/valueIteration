6a04768467149bce96368daab2947ddf27e38b85
Alec Fong <alecfong1@gmail.com>
2019-04-24 10:17:51 -0700

    skeleton code

---
+++ tester.py
@@ -0,0 +1,23 @@
+from project3cs360s2019 import main as run
+import pytest
+from shutil import copyfile
+
+@pytest.mark.parametrize("input_file_num,output_file_num", [
+    (0,0),
+    (1,1)
+])
+def test_files(input_file_num, output_file_num):
+    input_file_name = "dev_cases/input-{}.txt".format(input_file_num)
+    output_file_name = "dev_cases/output-{}.txt".format(output_file_num)
+
+    copyfile(input_file_name, "input.txt")
+
+    run()
+
+    res, expected = "", ""
+    with open(output_file_name, 'r') as f:
+        res = f.read().replace("\r", "")
+    with open("output.txt", 'r') as f:
+        expected = f.read()
+    
+    assert res == expected
